e03502140f904eff08b9ff54e6178f53329e707e
gabriel-ijk <gscheffler30@gmail.com>
2024-11-13 16:06:30 -0300

    banco hospedado

--- models/db.js
+++ models/db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'tcc_koryo'
+    host: 'mysql.infocimol.com.br',
+    user: 'infocimol05',
+    password: 'Koryotcc8448',
+    database: 'infocimol05'
 });
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'tcc_koryo'
+// });
 
 module.exports = pool.promise();
